f6db6a98cdbf18317002d7f27fb53039bff42a11
jusceliop <jusceliop@gmail.com>
2018-10-04 16:44:32 -0300

    Update alisson.sh
    https://raw.githubusercontent.com/jusceliop/install/master/start.sh

--- alisson.sh
+++ alisson.sh
@@ -5,7 +5,6 @@ apt install -y software-properties-common python-software-properties
 apt-add-repository ppa:ondrej/php
 apt update
 sudo apt -y install php7.2 php7.2-mysql php7.2-curl php7.2-zip php7.2-intl php7.2-xml php7.2-mb$
-
 sudo chmod -R 777 /etc/php/
 
 php -v
